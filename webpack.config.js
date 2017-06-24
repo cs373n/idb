b96ed3bf79cd1ae924a871ffa57725acfff9e3e2
root <root@ip-172-31-24-177.ec2.internal>
2017-06-24 19:54:32 +0000

    restructure environment, webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './app/index.js',
+	entry: './app/static/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index_bundle.js'
+		path: path.resolve(__dirname, './app/static/dist'),
+		filename: 'bundle.js'
 	},
 	module: {
 		rules: [
-		{ test: /\.(js)$/, use: 'babel-loader' },
+		{ test: /\.(jsx)$/, use: 'babel-loader' },
 		{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
 		]
 	},
 	plugins: [new HtmlWebpackPlugin({
-		template: 'app/index.html'
+		template: 'app/templates/index.html'
 	})]
 }
