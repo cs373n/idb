3ef0fcc643bd2018b4be2911c9e02c5cf7475608
dean carpenter <deancarpenter17@gmail.com>
2017-06-29 16:16:29 -0500

    add events homepage with events table

---
+++ app/static/components/Events.js
@@ -0,0 +1,69 @@
+var React = require('react');
+import { PageHeader } from 'react-bootstrap';
+var api = require('./api.js');
+var Table = require('./Table.js');
+var Card = require('./Card.js')
+
+var fixMargin = {
+	margin: '0'
+}
+
+class Events extends React.Component{
+	constructor(props) {
+	    super();
+	    this.state = {
+	      events: null
+    	};
+
+    	// Maybe.... this.updateChars = this.updateChars.bind(this);
+  	}
+
+	componentDidMount() {
+	    this.updateEvents(this.state.events)
+	}
+
+	updateEvents(events) {
+
+		this.setState(function() {
+			return {
+				events: events
+			}
+		});
+
+		api.getEvents()
+	      .then(function (events) {
+	        this.setState(function () {
+	          console.log(events)
+	          return {
+	            events: events
+	          }
+	        });
+	      }.bind(this));
+	}
+
+	createCards() {
+		var cardsArray = [];
+		var eventsCopy = this.state.events
+		for(var i = 0; i < eventsCopy.length; i++) {
+			cardsArray.push(<Card name={eventsCopy[i].fullName} img={eventsCopy[i].thumbnail.path+"/standard_xlarge.jpg"} />);
+		}
+		return cardsArray;
+	}
+
+	render(){
+		return(
+			<div className="container">
+				<PageHeader className="text-center" style={fixMargin}>EVENTS</PageHeader>
+
+				{!this.state.events
+		          ? <p>LOADING!</p>
+		          /* Table here */
+		          : <Table cards={this.createCards()}/>
+		        }
+			</div>
+		)
+	}
+
+}
+
+module.exports = Events;
