e171e479fca8005e50417324b2070e943b8368b0
dean carpenter <deancarpenter17@gmail.com>
2017-06-29 16:12:42 -0500

    add creators homepage with table grid

--- app/static/components/Creators.js
+++ app/static/components/Creators.js
@@ -1,13 +1,69 @@
 var React = require('react');
+import { PageHeader } from 'react-bootstrap';
+var api = require('./api.js');
+var Table = require('./Table.js');
+var Card = require('./Card.js')
+
+var fixMargin = {
+	margin: '0'
+}
 
 class Creators extends React.Component{
+	constructor(props) {
+	    super();
+	    this.state = {
+	      creators: null
+    	};
+
+    	// Maybe.... this.updateChars = this.updateChars.bind(this);
+  	}
+
+	componentDidMount() {
+	    this.updateCreators(this.state.creators)
+	}
+
+	updateCreators(creators) {
+
+		this.setState(function() {
+			return {
+				creators: creators
+			}
+		});
+
+		api.getCreators()
+	      .then(function (creators) {
+	        this.setState(function () {
+	          console.log(creators)
+	          return {
+	            creators: creators
+	          }
+	        });
+	      }.bind(this));
+	}
+
+	createCards() {
+		var cardsArray = [];
+		var creatorsCopy = this.state.creators
+		for(var i = 0; i < creatorsCopy.length; i++) {
+			cardsArray.push(<Card name={creatorsCopy[i].fullName} img={creatorsCopy[i].thumbnail.path+"/standard_xlarge.jpg"} />);
+		}
+		return cardsArray;
+	}
+
 	render(){
 		return(
-			<div className='container'>
-			CREATORS
+			<div className="container">
+				<PageHeader className="text-center" style={fixMargin}>CREATORS</PageHeader>
+
+				{!this.state.creators
+		          ? <p>LOADING!</p>
+		          /* Table here */
+		          : <Table cards={this.createCards()}/>
+		        }
 			</div>
 		)
 	}
+
 }
 
 module.exports = Creators;
