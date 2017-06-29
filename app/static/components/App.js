c655f0e58fa2717adfc437de920e44abb6782a23
dean carpenter <deancarpenter17@gmail.com>
2017-06-29 11:55:06 -0500

    add React routing logic

--- app/static/components/App.js
+++ app/static/components/App.js
@@ -3,9 +3,12 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var NavBar = require('./NavBar.js');
 var Home = require('./Home.js');
+var Characters = require('./Characters.js')
 var Creators = require('./Creators.js');
+
 
 class App extends React.Component {
 	render() {
@@ -13,8 +16,14 @@ class App extends React.Component {
 		<Router>
 			<div>
 				<NavBar />
-				<Route exact path='/' component={Home} />
-				<Route path='/creators' component={Creators} />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/characters' component={Characters} />
+					<Route path='/creators' component={Creators} />
+					<Route render={function() {
+						return <p>Page Not Found!</p>
+					}} />
+				</Switch>
 			</div>
 		</Router>
 		)
