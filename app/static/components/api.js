134bc95716a66b578ab3724ccfa0e35a3e58de6a
deancarpenter17 <deancarpenter17@gmail.com>
2017-06-30 13:45:30 -0500

    add axios request for our database api

--- app/static/components/api.js
+++ app/static/components/api.js
@@ -2,10 +2,12 @@ var axios = require('axios');
 
 var urlEndPoint = "http://gateway.marvel.com/v1/public/";
 var ts = new Date().getTime();
-var privateKey = ""
-var publicKey = "";
+var privateKey = "c0cca37e9e919d673f13d91eadaaa1c7c683c745"
+var publicKey = "8097970cb5bc96d3dd151e9f983b76c8";
 var md5 = require('md5');
 var md5hash = md5(ts+privateKey+publicKey);
+
+var ourURL = "http://52.201.212.202/api/character";
 
 // Construct the required URL params
 var params = "?ts=" + ts + "&apikey=" + publicKey + 
@@ -32,7 +34,7 @@ module.exports = {
 	return axios.get(encodedURI)
 	    .then(function (response) {
 
-	      // data.data?? .....y tho
+	      
 	      return response.data.data.results; // Return an array of creators
 	   	});
   	},
@@ -43,7 +45,7 @@ module.exports = {
 	return axios.get(encodedURI)
 	    .then(function (response) {
 
-	      // data.data?? .....y tho
+	      
 	      return response.data.data.results; // Return an array of creators
 	   	});
   	},
@@ -54,8 +56,27 @@ module.exports = {
 	return axios.get(encodedURI)
 	    .then(function (response) {
 
-	      // data.data?? .....y tho
+	      
 	      return response.data.data.results; // Return an array of creators
 	   	});
-  	}
+  	},
+
+  	getOurCharacters: function () {
+	    var encodedURI = window.encodeURI(ourURL);
+
+		 return axios.get(encodedURI, {
+		 	headers: {
+		 		'Content-Type': 'application/json'
+		 	}
+		 }
+		 	)
+	    .then(function (response) {
+	      	console.log(response.data.objects)
+	      
+      
+   		return response.data.objects; // Return an array of creators
+		}).catch(function (error) {
+    		console.log(error);
+  		});
+	}
 };
