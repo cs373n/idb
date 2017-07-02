c2be8e11ae9fe9a4cbe82855ec09fc3ca6c74cef
saketksingh <saketsingh2018@gmail.com>
2017-07-02 17:34:25 -0500

    Run autopep8

--- app/models.py
+++ app/models.py
@@ -5,63 +5,75 @@ from sqlalchemy.schema import ForeignKey
 db = SQLAlchemy()
 
 # many-to-many relationship tables
-creator_series = db.Table('creator_series', 
-    db.Column('creator_id', db.Integer, ForeignKey('creator.id')), 
-    db.Column('series_id', db.Integer, ForeignKey('series.id'))
-)
+creator_series = db.Table('creator_series',
+                          db.Column(
+                              'creator_id', db.Integer, ForeignKey('creator.id')),
+                          db.Column(
+                          'series_id', db.Integer, ForeignKey('series.id'))
+                          )
 creator_event = db.Table('creator_event',
-    db.Column('creator_id', db.Integer, ForeignKey('creator.id')),
-    db.Column('event_id', db.Integer, ForeignKey('event.id'))
-)
+                         db.Column(
+                             'creator_id', db.Integer, ForeignKey('creator.id')),
+                         db.Column(
+                         'event_id', db.Integer, ForeignKey('event.id'))
+                         )
 character_series = db.Table('character_series',
-    db.Column('character_id', db.Integer, ForeignKey('character.id')),
-    db.Column('series_id', db.Integer, ForeignKey('series.id'))
-)
+                            db.Column(
+                                'character_id', db.Integer, ForeignKey('character.id')),
+                            db.Column(
+                            'series_id', db.Integer, ForeignKey('series.id'))
+                            )
 character_event = db.Table('character_event',
-    db.Column('character_id', db.Integer, ForeignKey('character.id')),
-    db.Column('event_id', db.Integer, ForeignKey('event.id'))
-)
+                           db.Column(
+                               'character_id', db.Integer, ForeignKey('character.id')),
+                           db.Column(
+                           'event_id', db.Integer, ForeignKey('event.id'))
+                           )
 event_series = db.Table('event_series',
-    db.Column('event_id', db.Integer, ForeignKey('event.id')),
-    db.Column('series_id', db.Integer, ForeignKey('series.id'))
-)
+                        db.Column(
+                            'event_id', db.Integer, ForeignKey('event.id')),
+                        db.Column(
+                        'series_id', db.Integer, ForeignKey('series.id'))
+                        )
 
 
-
-# Models a series object. 
+# Models a series object.
 class Series(db.Model):
-    id = db.Column(db.Integer, primary_key=True)	
-    title = db.Column(db.String(150))	
-    desc = db.Column(db.String(1300))	 
-    start = db.Column(db.Integer)		
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    desc = db.Column(db.String(1300))
+    start = db.Column(db.Integer)
     end = db.Column(db.Integer)
-    img = db.Column(db.String(150)) 
+    img = db.Column(db.String(150))
     num_creators = db.Column(db.Integer)
     num_characters = db.Column(db.Integer)
     num_comics = db.Column(db.Integer)
     num_events = db.Column(db.Integer)
-    
-    characters = db.relationship('Character', secondary=character_series, backref=db.backref('series', lazy='dynamic'))
-    creators = db.relationship('Creator', secondary=creator_series, backref=db.backref('series', lazy='dynamic'))
-    events = db.relationship('Event', secondary=event_series, backref=db.backref('series', lazy='dynamic'))
-    
+
+    characters = db.relationship(
+        'Character', secondary=character_series, backref=db.backref('series', lazy='dynamic'))
+    creators = db.relationship(
+        'Creator', secondary=creator_series, backref=db.backref('series', lazy='dynamic'))
+    events = db.relationship(
+        'Event', secondary=event_series, backref=db.backref('series', lazy='dynamic'))
+
     def __init__(self, id, title, desc, start, end, img, num_creators, num_characters, num_comics, num_events):
-    	assert title != ""
+        assert title != ""
         assert img != ""
         assert start > 0
         assert end > 0
 
-    	self.id = id 
-    	self.title = title
-    	self.desc = desc
-    	self.start = start
-    	self.end = end
+        self.id = id
+        self.title = title
+        self.desc = desc
+        self.start = start
+        self.end = end
         self.img = img
         self.num_creators = num_creators
         self.num_characters = num_characters
         self.num_comics = num_comics
         self.num_events = num_events
-    
+
 
 # Models a Character object
 class Character(db.Model):
@@ -72,21 +84,20 @@ class Character(db.Model):
     num_comics = db.Column(db.Integer)
     num_series = db.Column(db.Integer)
     num_events = db.Column(db.Integer)
-    
-    events = db.relationship('Event', secondary=character_event, backref=db.backref('characters', lazy='dynamic'))
 
+    events = db.relationship(
+        'Event', secondary=character_event, backref=db.backref('characters', lazy='dynamic'))
 
     def __init__(self, id, name, desc, img, num_comics, num_series, num_events):
-    	assert name != ""
+        assert name != ""
 
-    	self.id = id        
-    	self.name = name
-    	self.desc = desc
-    	self.img = img
+        self.id = id
+        self.name = name
+        self.desc = desc
+        self.img = img
         self.num_comics = num_comics
         self.num_series = num_series
         self.num_events = num_events
-				
 
 
 # Models an Event object
@@ -100,15 +111,15 @@ class Event(db.Model):
     num_comics = db.Column(db.Integer)
     num_series = db.Column(db.Integer)
 
-    creators = db.relationship('Creator', secondary=creator_event, backref=db.backref('events', lazy='dynamic'))
-
+    creators = db.relationship(
+        'Creator', secondary=creator_event, backref=db.backref('events', lazy='dynamic'))
 
     def __init__(self, id, title, desc, img, num_creators, num_characters, num_comics, num_series):
 
-    	self.id = id
-    	self.title = title
-    	self.desc = desc
-    	self.img = img
+        self.id = id
+        self.title = title
+        self.desc = desc
+        self.img = img
         self.num_creators = num_creators
         self.num_characters = num_characters
         self.num_comics = num_comics
@@ -124,14 +135,11 @@ class Creator(db.Model):
     num_series = db.Column(db.Integer)
     num_events = db.Column(db.Integer)
 
-
     def __init__(self, id, full_name, img, num_comics, num_series, num_events):
-        
 
-    	self.id = id
-    	self.full_name = full_name
-    	self.img = img
+        self.id = id
+        self.full_name = full_name
+        self.img = img
         self.num_comics = num_comics
         self.num_series = num_series
         self.num_events = num_events
-
