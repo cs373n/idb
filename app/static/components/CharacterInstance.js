215cbf16d5cfee3114efac194fd46eac8a8f4b6f
deancarpenter17 <deancarpenter17@gmail.com>
2017-07-02 17:55:07 -0500

    include null checks on array

--- app/static/components/CharacterInstance.js
+++ app/static/components/CharacterInstance.js
@@ -51,19 +51,21 @@ class CharacterInstance extends React.Component {
 	createSeriesCards() {
 		var cardsArray = [];
 		var assocSeries = this.state.character.series;
-		for(var i = 0; i < assocSeries.length; i++) {
-			if(assocSeries[i].img && assocSeries[i].img != "") {
-				assocSeries[i].img = assocSeries[i].img.slice(0, -4) + "/standard_xlarge.jpg";
-				cardsArray.push(<Card modelLink="/seriesInstance" 
-								      modelInstance={assocSeries[i]}/>);
-				console.log(assocSeries[i]);
-			}
+		if(assocSeries) {
+			for(var i = 0; i < assocSeries.length; i++) {
+				if(assocSeries[i].img && assocSeries[i].img != "") {
+					assocSeries[i].img = assocSeries[i].img.slice(0, -4) + "/standard_xlarge.jpg";
+					cardsArray.push(<Card modelLink="/seriesInstance" 
+									      modelInstance={assocSeries[i]}/>);
+					console.log(assocSeries[i]);
+				}
 
-			else {
-				assocSeries[i].img = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
-				console.log(assocSeries[i]);
-				cardsArray.push(<Card modelLink="/seriesInstance" 
-								 	  modelInstance={assocSeries[i]} />);
+				else {
+					assocSeries[i].img = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
+					console.log(assocSeries[i]);
+					cardsArray.push(<Card modelLink="/seriesInstance" 
+									 	  modelInstance={assocSeries[i]} />);
+				}
 			}
 		}
 		return cardsArray;
@@ -72,19 +74,21 @@ class CharacterInstance extends React.Component {
 	createEventsCards() {
 		var cardsArray = [];
 		var assocEvents = this.state.character.events;
-		for(var i = 0; i < assocEvents.length; i++) {
-			if(assocEvents[i].img && assocEvents[i].img != "") {
-				assocEvents[i].img = assocEvents[i].img.slice(0, -4) + "/standard_xlarge.jpg";
-				cardsArray.push(<Card modelLink="/eventInstance" 
-								      modelInstance={assocEvents[i]}/>);
-				console.log(assocEvents[i]);
-			}
+		if(assocEvents) {
+			for(var i = 0; i < assocEvents.length; i++) {
+				if(assocEvents[i].img && assocEvents[i].img != "") {
+					assocEvents[i].img = assocEvents[i].img.slice(0, -4) + "/standard_xlarge.jpg";
+					cardsArray.push(<Card modelLink="/eventInstance" 
+									      modelInstance={assocEvents[i]}/>);
+					console.log(assocEvents[i]);
+				}
 
-			else {
-				assocEvents[i].img = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
-				console.log(assocEvents[i]);
-				cardsArray.push(<Card modelLink="/eventInstance" 
-								 	  modelInstance={assocEvents[i]} />);
+				else {
+					assocEvents[i].img = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
+					console.log(assocEvents[i]);
+					cardsArray.push(<Card modelLink="/eventInstance" 
+									 	  modelInstance={assocEvents[i]} />);
+				}
 			}
 		}
 		return cardsArray;
