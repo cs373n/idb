5f2c62ab2caee8ecb50a154fe0fb5373b7bd219f
deancarpenter17 <deancarpenter17@gmail.com>
2017-07-02 18:58:29 -0500

    unignore webpack dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 var path = require('path');
 var webpack = require('webpack');
-//var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './app/static/components/App.js',
 	output: {
 		path: path.resolve(__dirname, './app/static/dist'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		publicPath: '/'
 	},
 	module: {
 		rules: [
 		{ test: /\.(js)$/, use: 'babel-loader' },
 		{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
 		]
-	}
-
-	/*plugins: [new HtmlWebpackPlugin({
+	},
+	devServer: {
+		historyApiFallback: true
+	},
+	plugins: [new HtmlWebpackPlugin({
 		template: 'app/templates/index.html'
-		})]*/
+		})]
 }
