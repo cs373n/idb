992825097a5c57e25ec788f92b3f97fe70b2eeaa
Davis95 <pauldavis95@hotmail.com>
2017-07-03 19:39:04 -0500

    Added button style.

--- app/static/components/App.js
+++ app/static/components/App.js
@@ -26,38 +26,58 @@ var Card = require('./Card.js');
 var Footer = require('./Footer.js');
 var About = require('./About.js');
 
+import { Button, Navbar } from 'react-bootstrap';
+import { bootstrapUtils } from 'react-bootstrap/lib/utils';
+
+bootstrapUtils.addStyle(Button,'red');
+bootstrapUtils.addStyle(Navbar, ['red']);
+
+
 class App extends React.Component {
 	render() {
 		return (
-		<Router>
 			<div>
-				<NavBar />
-				<div>
-					<Switch>
-						<Route exact path='/' component={Home} />
-						
-						<Route path='/characters' component={Characters} />
-						<Route path='/characterInstance/:charID' component={CharacterInstance} />
+				<style type="text/css">{`
+			    .btn-red {
+			        background-color: red;
+			        color: white;
+			    }
+			    .navbar-red{
+			    	background-color: red;
+			    	color: white
+			    }
+			    `}
+			    </style>
+				<Router>
+					<div>
+						<NavBar />
+						<div>
+							<Switch>
+								<Route exact path='/' component={Home} />
+								
+								<Route path='/characters' component={Characters} />
+								<Route path='/characterInstance/:charID' component={CharacterInstance} />
 
-						<Route path='/events' component={Events} />
-						<Route path='/eventInstance/:eventID' component={EventInstance} />
+								<Route path='/events' component={Events} />
+								<Route path='/eventInstance/:eventID' component={EventInstance} />
 
-						<Route path='/series' component={Series} />
-						<Route path='/seriesInstance/:seriesID' component={SeriesInstance} />
+								<Route path='/series' component={Series} />
+								<Route path='/seriesInstance/:seriesID' component={SeriesInstance} />
 
-						<Route path='/creators' component={Creators} />
-						<Route path='/creatorInstance/:creatorID' component={CreatorInstance} />
+								<Route path='/creators' component={Creators} />
+								<Route path='/creatorInstance/:creatorID' component={CreatorInstance} />
 
-						<Route path='/about' component={About} />
+								<Route path='/about' component={About} />
 
-						<Route render={function() {
-							return <p>Page Not Found!</p>
-						}} />
-					</Switch>
-				<Footer/>
-				</div>
+								<Route render={function() {
+									return <p>Page Not Found!</p>
+								}} />
+							</Switch>
+						<Footer/>
+						</div>
+					</div>
+				</Router>
 			</div>
-		</Router>
 		)
 	}
 }
