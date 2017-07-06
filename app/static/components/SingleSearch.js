13c21dbce57ba4c799c73834915ee549f0260668
Davis95 <pauldavis95@hotmail.com>
2017-07-06 18:04:37 -0500

    Component to make requests and render results for a single word search (does not rerender on second search input).

---
+++ app/static/components/SingleSearch.js
@@ -0,0 +1,175 @@
+var React = require('react');
+var api = require('./api.js');
+var Table = require('./Table.js');
+var SearchCard = require('./SearchCard.js');
+import { PageHeader, Pagination, Tabs, Tab } from 'react-bootstrap';
+
+var orderByAsc = [{'field': 'name', 'direction': 'asc'}];
+
+class SingleSearch extends React.Component {
+	constructor(props) {
+		super();
+		this.state = {
+			searchResults: null,
+			activePage: 1,
+	      	numPages: 0
+		}
+
+		this.updateSearchResults = this.updateSearchResults.bind(this);
+		this.createSearchCards = this.createSearchCards.bind(this);
+		this.handlePageSelect = this.handlePageSelect.bind(this);
+		this.loadTable = this.loadTable.bind(this);
+		this.buildFilter = this.buildFilter.bind(this);
+	}
+
+	componentWillMount() {
+	    this.updateSearchResults(null);
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps.modelType != this.props.modelType || (nextProps.searchString != this.props.searchString)) {
+			this.updateSearchResults(null);
+		}
+
+	}
+
+	updateSearchResults(searchResults) {
+		const { modelType } = this.props;
+		var filter;
+
+		this.setState(function() {
+			return {
+				searchResults: searchResults
+			}
+		});
+
+		if(modelType === 'character') {
+				
+				filter = this.buildFilter();
+
+				api.getCharacters(this.state.activePage, filter, orderByAsc)
+			      .then(function (chars) {
+			        this.setState(function () {
+			          return {
+			            searchResults: chars.objects,
+			            numPages: chars.total_pages
+			          }
+			        });
+			      }.bind(this));
+			}
+			else if(modelType === 'event'){
+
+				filter = this.buildFilter();
+
+				api.getEvents(this.state.activePage, filter, {})
+			      .then(function (chars) {
+			        this.setState(function () {
+			          return {
+			            searchResults: chars.objects,
+			            numPages: chars.total_pages
+			          }
+			        });
+			      }.bind(this));
+			}
+			else if(modelType === 'series'){
+
+				filter = this.buildFilter();
+
+				api.getSeries(this.state.activePage, filter, {})
+			      .then(function (chars) {
+			        this.setState(function () {
+			          return {
+			            searchResults: chars.objects,
+			            numPages: chars.total_pages
+			          }
+			        });
+			      }.bind(this));
+			}
+			else if(modelType === 'creator'){
+
+				filter = this.buildFilter();
+
+				api.getCreators(this.state.activePage, filter, {})
+			      .then(function (chars) {
+			        this.setState(function () {
+			          return {
+			            searchResults: chars.objects,
+			            numPages: chars.total_pages
+			          }
+			        });
+			      }.bind(this));
+			}
+	}
+
+	buildFilter() {
+		const { modelType } = this.props;
+		const { searchString } = this.props;
+		if(modelType === 'character') {
+			return [{"or": [{"name": "name", "op": "ilike", "val": "%" + searchString + "%"}, 
+							{"name": "desc", "op": "ilike", "val": "%" + searchString + "%"}]}];
+			//return [{'name': 'desc','op': 'any', 'val': searchString}];
+		}
+		else if(modelType === 'event' || modelType === 'series'){
+			return [{"or": [{"name": "title", "op": "ilike", "val": "%" + searchString + "%"}, 
+							{"name": "desc", "op": "ilike", "val": "%" + searchString + "%"}]}];
+		}
+		else if(modelType === 'creator'){
+			return [{"name": "full_name", "op": "ilike", "val": "%" + searchString + "%"}];
+		}
+	}
+
+	handlePageSelect(eventKey){
+		this.setState({activePage: eventKey}, function () {
+			this.updateSearchResults(null);
+		});
+	}
+
+	createSearchCards(){
+		var cardsArray = [];
+		var { searchResults } = this.state;
+		var modelLink = "/" + this.props.modelType + "Instance";
+		for(var i = 0; i < searchResults.length; i++) {
+				cardsArray.push(<SearchCard modelLink={modelLink}
+								      		modelInstance={searchResults[i]} 
+								      		modelType={this.props.modelType}/>);
+		}
+		return cardsArray;
+	}
+
+	loadTable(){
+		if(!this.state.searchResults){
+            return <p>LOADING!</p>;
+        }
+        else if(this.state.searchResults[0] === {}){
+        	return <p>No results match that search criteria.</p>
+        }   
+        else {
+         	return (
+         		<div>
+	         		<Table cards={this.createSearchCards()}/>
+	          		<Pagination
+			       	prev
+			        next
+			        first
+			        last
+			        ellipsis
+			        boundaryLinks
+			        items={this.state.numPages}
+			        maxButtons={5}
+			        activePage={this.state.activePage}
+			        onSelect={this.handlePageSelect} />
+		    	</div>
+		    );
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				{this.loadTable()}
+			</div>
+		);
+	}
+}
+
+module.exports = SingleSearch;
