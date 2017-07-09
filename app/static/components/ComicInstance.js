c18ada3a571c3f9425a1d37e1cee68c548cecb94
Davis95 <pauldavis95@hotmail.com>
2017-07-09 04:07:36 -0500

    Implemented home and instance pages for Comics.

---
+++ app/static/components/ComicInstance.js
@@ -0,0 +1,122 @@
+var React = require('react');
+var Table = require('./Table.js');
+var Card = require('./Card.js');
+var api = require('./api.js');
+import { PageHeader, Row, Col, Grid, Tab, Tabs } from 'react-bootstrap';
+
+var h2Font = {
+	fontSize: '20px',
+};
+
+class ComicInstance extends React.Component {
+	constructor(props) {
+	    super();
+	    this.state = {
+	      comic: null
+    	};
+  	}
+
+	componentDidMount() {
+	    this.updateComic(this.state.comic);
+	}
+
+	updateComic(comic) {
+		var comicID = this.props.match.params.comicID;
+
+		this.setState(function() {
+			return {
+				comic: comic
+			}
+		});
+
+		api.getComic(comicID)
+	      .then(function (comic) {
+	        this.setState(function () {
+	          return {
+	            comic: comic
+	          }
+	        });
+	      }.bind(this));
+	}
+
+	fixImage() {
+		const { comic } = this.state;
+
+		if(comic.img && comic.img != "") {
+			return comic.img.slice(0, -4) + "/portrait_incredible.jpg";
+		}
+		return "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/portrait_incredible.jpg";
+	}
+
+	createSeriesCards() {
+		var cardsArray = [];
+		var assocSeries = this.state.comic.series;
+		if(assocSeries) {
+			for(var i = 0; i < assocSeries.length; i++) {
+				cardsArray.push(<Card modelLink="/seriesInstance" modelInstance={assocSeries[i]}/>);
+			}
+		}
+		return cardsArray;
+	}
+
+	createEventsCards() {
+		var cardsArray = [];
+		var assocEvents = this.state.comic.events;
+		if(assocEvents) {
+			for(var i = 0; i < assocEvents.length; i++) {
+				cardsArray.push(<Card modelLink="/eventInstance" modelInstance={assocEvents[i]}/>);
+			}
+		}
+		return cardsArray;
+	}
+
+	render() {
+		const { comic } = this.state;
+
+		if(!comic) {
+			return <p>LOADING!</p>
+		}
+		else {
+
+			return (
+				<div className="container">
+					<PageHeader className="text-left">{comic.title}</PageHeader>
+					<Grid>
+						<Row>
+							<Col md={3}>
+								<img className="img-rounded img-responsive" src={this.fixImage()} alt={comic.title}/>
+							</Col>
+
+							<Col className="text-left" md={9}>
+								<PageHeader style={h2Font}>Description</PageHeader>
+								<p>{(comic.desc == null || comic.desc == "") ? "Description not available." : comic.desc}</p>
+								<PageHeader style={h2Font}>Attributes</PageHeader>
+								<ul>
+									<li>Appears in the Series: {comic.series[0].title}</li>
+									<li>Appears in {comic.events.length} Events</li>
+								</ul>
+							</Col>
+						</Row>
+					</Grid>
+					
+					<br/>
+
+					<Tabs bsStyle="tabs" defaultActiveKey={1}>
+	    				<Tab eventKey={1} title="FEATURED SERIES">
+	    					<br/>
+	    					<Table cards={this.createSeriesCards()}/>
+	    				</Tab>
+	    				<Tab eventKey={2} title="FEATURED EVENTS">
+	    					<br/>
+	    					<Table cards={this.createEventsCards()}/>
+	    				</Tab>	
+	 				 </Tabs>
+
+				</div>
+			)
+		}
+
+	}
+}
+
+module.exports = ComicInstance;
---
+++ app/static/components/Comics.js
@@ -0,0 +1,199 @@
+var React = require('react');
+var api = require('./api.js');
+var Table = require('./Table.js');
+var Card = require('./Card.js');
+import { PageHeader, Pagination, Button, 
+		 ButtonGroup, ButtonToolbar,
+		 Grid, Row, Col } from 'react-bootstrap';
+
+var fixMargin = {
+	margin: '0'
+}
+
+var imgNotFound = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
+var photoFilter = [{'name': 'img','op': 'does_not_equal', 'val': imgNotFound}];
+var descFilter = [{'name': 'desc','op': '!=', 'val': ''}];
+var orderByAsc = [{'field': 'title', 'direction': 'asc'}];
+var orderByDsc = [{'field': 'title', 'direction': 'desc'}];
+
+class Comics extends React.Component{
+
+	constructor(props) {
+	    super();
+	    this.state = {
+	      comics: null,
+	      activePage: 1,
+	      numPages: 0,
+	      hasPhoto: false,
+	      hasDesc: false,
+	      sortAsc: true,
+    	};
+
+    	this.handleSelect = this.handleSelect.bind(this); //pls work
+    	this.updateComics = this.updateComics.bind(this); //pls work
+    	this.applyFilter = this.applyFilter.bind(this);
+    	this.loadTable = this.loadTable.bind(this);
+  	}
+
+	componentWillMount() {
+	    this.updateComics(this.state.comics);
+	}
+
+	updateComics(comics) {
+		var filter;
+		var orderBy;
+		this.setState(function() {
+			return {
+				comics: null
+			}
+		});
+
+		if(this.state.hasPhoto) {
+			filter = photoFilter;
+		}
+		else if(this.state.hasDesc) {
+			filter = descFilter;
+		}
+
+		if(this.state.sortAsc){
+			orderBy = orderByAsc;
+		}
+		else{
+			orderBy = orderByDsc;
+		}
+
+		api.getComics(this.state.activePage, filter, orderBy)
+	      .then(function (comics) {
+	        this.setState(function () {
+	          return {
+	            comics: comics.objects,
+	            numPages: comics.total_pages
+	          }
+	        });
+	      }.bind(this));
+	}
+
+	handleSelect(eventKey){
+		this.setState({activePage: eventKey}, function () {
+			this.updateComics(null);
+		});
+	}
+
+	applyFilter(filterKey){
+	    if(filterKey === 1) {
+	    	this.setState({
+	    		hasPhoto: true,
+	    		hasDesc: false
+	    	}, function() {
+	    		this.updateComics(null);
+	    	});
+	    }
+	    else if(filterKey === 2){
+	    	this.setState({
+	    		hasPhoto: false,
+	    		hasDesc: true
+	    	}, function() {
+	    		this.updateComics(null);
+	    	});
+	    }
+	}
+
+	applySort(sortKey){
+		if(sortKey === 1){
+			this.setState({
+				sortAsc: true
+			}, function(){
+				this.updateComics(null);
+			});
+		}
+		else if(sortKey === 2){
+	    	this.setState({
+	    		sortAsc: false
+	    	}, function() {
+	    		this.updateComics(null);
+	    	});
+	    }
+		
+	}
+
+	createCards() {
+		var cardsArray = [];
+		var { comics } = this.state;
+		for(var i = 0; i < comics.length; i++) {
+				cardsArray.push(<Card modelLink="/comicInstance" 
+								      modelInstance={comics[i]} />);
+		}
+		return cardsArray;
+	}
+
+	loadTable(){
+		if(!this.state.comics){
+            return <p>LOADING!</p>;
+        }   
+        else{
+         	return (
+         		<div className="text-center">
+	         		<Table cards={this.createCards()}/>
+	          		<Pagination
+			       	prev
+			        next
+			        first
+			        last
+			        ellipsis
+			        boundaryLinks
+			        items={this.state.numPages}
+			        maxButtons={5}
+			        activePage={this.state.activePage}
+			        onSelect={this.handleSelect} />
+		    	</div>
+		    );
+		}
+	}
+
+	render(){
+		return(
+			<div className="container">
+				<PageHeader className="text-center" style={fixMargin}>COMICS</PageHeader>
+				<Grid>
+					<Row>
+						<Col md={3}/>
+						<Col md={6}>
+							<ul className="list-inline list-unstyled">
+								<li>
+									<h3 className="text-center">FILTER BY:</h3>
+									<ButtonToolbar>
+										<Button bsStyle="red" onClick={() => this.applyFilter(1)}>
+												Photo Available
+										</Button>
+										<Button bsStyle="red" onClick={() => this.applyFilter(2)}>
+												Description Available
+										</Button>
+									</ButtonToolbar>
+									{this.state.hasPhoto ? <p>Photo Filter Applied</p> : <p/>}
+									{this.state.hasDesc ? <p>Description Filter Applied</p> : <p/>}
+								</li>
+								<li className="pull-right">
+									<h3 className="text-center">SORT BY:</h3>
+									<ButtonToolbar>
+										<Button bsStyle="red" onClick={() => this.applySort(1)}>
+												Ascending
+										</Button>
+										<Button bsStyle="red" onClick={() => this.applySort(2)}>
+												Descending
+										</Button>
+									</ButtonToolbar>
+								</li>
+							</ul>
+						</Col>
+						<Col md={3}/>
+					</Row>
+				</Grid>
+				<PageHeader/> {/*Makes line across screen*/}
+				
+				{this.loadTable()}
+			</div>
+		)
+	}
+}
+
+module.exports = Comics;
