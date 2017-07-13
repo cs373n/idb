243358f28d0fcede267809f89af6f61edde9a068
Davis95 <pauldavis95@hotmail.com>
2017-07-13 13:01:22 -0500

    Home page for contributing to site content.

---
+++ app/static/components/Contribute.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+
+class Contribute extends React.Component{
+	render(){
+		return(
+			<div>
+				Sup bitches
+			</div>
+		)
+	}
+}
+
+module.exports = Contribute;
